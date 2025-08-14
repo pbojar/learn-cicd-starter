cec0e7eef9e5e4fa2c31baf5661f81c5b8a128d1
pbojar <preston.bowesjaros@gmail.com>
2025-08-14 16:55:28 -0400

    Auth tests with forced failure to test CI.

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	noFailHeader := http.Header{}
+	noFailHeader.Add("Authorization", "ApiKey 123456789")
+	malformedHeader := http.Header{}
+	malformedHeader.Add("Authorization", "No Key")
+	tests := map[string]struct {
+		input         http.Header
+		expected      string
+		expectedError string
+	}{
+		"no fail": {
+			input:    noFailHeader,
+			expected: "123456789 ForceFail",
+		},
+		"empty header": {
+			input:         http.Header{},
+			expected:      "",
+			expectedError: "no authorization header included",
+		},
+		"malformed header": {
+			input:         malformedHeader,
+			expected:      "",
+			expectedError: "malformed authorization header",
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			actual, err := GetAPIKey(tc.input)
+			diffResult := cmp.Diff(tc.expected, actual)
+			if diffResult != "" {
+				t.Fatal(diffResult)
+			}
+			if err != nil {
+				diffErr := cmp.Diff(tc.expectedError, err.Error())
+				if diffErr != "" {
+					t.Fatal(diffErr)
+				}
+			}
+		})
+	}
+}
